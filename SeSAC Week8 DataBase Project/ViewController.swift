1be3f1d20aff063da702f2322bd1a190c4669e3c
roargenie <105975078+roargenie@users.noreply.github.com>
2022-08-22 21:04:58 +0900

    Initial Commit

---
+++ SeSAC Week8 DataBase Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC Week8 DataBase Project
+//
+//  Created by 이명진 on 2022/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
